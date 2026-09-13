c9197abb23ee6a367408b907e25c66b909e0ebc9
Bahram2006 <yardam034@gmail.com>
2026-09-13 23:29:25 +0500

    feat(appointments): wire AppointmentsModule and register in AppModule

--- src/modules/appointments/appointments.module.ts
+++ src/modules/appointments/appointments.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AppointmentsService } from './services/appointments.service';
+import { AppointmentsController } from './controllers/appointments.controller';
 
-@Module({})
-export class AppointmentsModule {}
+@Module({
+    controllers: [AppointmentsController],
+    providers: [AppointmentsService],
+    exports: [AppointmentsService],
+})
+export class AppointmentsModule { }
